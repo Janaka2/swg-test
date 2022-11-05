6bc36aea2986c714107d13cf376f79c57590202f
Janaka Premathilaka <janaka2@gmail.com>
2022-11-05 22:45:32 +0100

    Initial commit

---
+++ src/main/java/com/jk/api/swgtest/SwgTestApplication.java
@@ -0,0 +1,13 @@
+package com.jk.api.swgtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwgTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/api/swgtest/SwgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.api.swgtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwgTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
